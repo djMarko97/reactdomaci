228ce752f2d38dd27c793daae22bf2d2748f5dd8
MARKO DJORDJEVIC <djordjevic.marko373@gmail.com>
2021-08-26 13:01:51 +0200

    user settings komponenta

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,11 +1,14 @@
 import TopBar from "./components/topbar/TopBar";
 import Write from "./pages/home/write/Write";
+import Settings from "./pages/home/settings/Settings";
+
 
 function App() {
   return (
     <>
       <TopBar />
-      <Write />
+      <Settings />
+      
     </>
   );
 }
---
+++ react-app/src/pages/home/settings/Settings.jsx
@@ -0,0 +1,33 @@
+import "./settings.css"
+import Sidebar from "../../../components/sidebar/Sidebar"
+
+export default function Settings() {
+    return (
+        <div className="settings">
+            <div className="settingsWrapper">
+                <div className="settingsTitle">
+                    <span className="settingsUpdateTitle">Update Your Account</span>
+                    <span className="settingsDeleteTitle">Delete Account</span>
+                </div>
+                <form className="settingsForm">
+                    <label>Profile Picture</label>
+                    <div className="settingsPP">
+                        <img src="https://img.theculturetrip.com/wp-content/uploads/2016/08/pl_launch_web_assets-02.jpg" alt="" />
+                        <label htmlFor="fileInput">
+                        <i className="settingsPPIcon far fa-user-circle"></i>
+                        </label>
+                        <input type="file" id="fileInput" style={{display:"none"}} />
+                    </div>
+                    <label>Username</label>
+                    <input type="text" placeholder="Marko"/>
+                    <label>Email</label>
+                    <input type="email" placeholder="marko@gmail.com"/>
+                    <label>Password</label>
+                    <input type="password"/>
+                    <button className="settingsSubmit">Update</button>
+                </form>
+            </div>
+            <Sidebar />
+        </div>
+    )
+}
