0cdd79b28e28da310d5200526c3c968c3f076a2e
MARKO DJORDJEVIC <djordjevic.marko373@gmail.com>
2021-08-26 13:55:28 +0200

    register komponenta

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,13 +2,14 @@ import TopBar from "./components/topbar/TopBar";
 import Write from "./pages/home/write/Write";
 import Settings from "./pages/home/settings/Settings";
 import Login from "./pages/home/login/Login";
+import Register from "./pages/home/register/Register";
 
 
 function App() {
   return (
     <>
       <TopBar />
-      <Login />
+      <Register />
       
     </>
   );
